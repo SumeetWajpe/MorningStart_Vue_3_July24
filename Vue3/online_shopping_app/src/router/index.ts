40f73051cb21cc055d0b303bb525248704e1d771
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-07-05 11:13:20 +0530

    ✨ Nested Routes

--- Vue3/online_shopping_app/src/router/index.ts
+++ Vue3/online_shopping_app/src/router/index.ts
@@ -2,19 +2,36 @@ import ListOfCourses from "@/components/ListOfCourses.vue";
 import Posts from "@/components/Posts.vue";
 import PostDetails from "@/components/PostDetails.vue";
 import CourseDetails from "@/components/CourseDetails.vue";
+import Dashboard from "@/components/Dashboard.vue";
 
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [
-  { path: "/", component: ListOfCourses },
-  {
-    path: "/coursedetails/:id",
-    name: "coursedetails",
-    component: CourseDetails,
-  },
+// const routes = [
+//   { path: "/", component: ListOfCourses },
+//   {
+// path: "/coursedetails/:id",
+// name: "coursedetails",
+// component: CourseDetails,
+//   },
 
-  { path: "/posts", component: Posts },
-  { path: "/postdetails/:id", component: PostDetails },
+//   { path: "/posts", component: Posts },
+//   { path: "/postdetails/:id", component: PostDetails },
+// ];
+
+// Nested Routes
+const routes = [
+  {
+    path: "/dashboard",
+    component: Dashboard,
+    children: [
+      { path: "", component: ListOfCourses },
+      {
+        path: "coursedetails/:id",
+        name: "coursedetails",
+        component: CourseDetails,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
